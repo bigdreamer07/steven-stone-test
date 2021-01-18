50587e31559a2295e292ffbe855e1c0f3558f0b3
bigdreamer07 <lancerstar2020@outlook.com>
2021-01-18 23:28:38 +0800

    subscribe updated

--- src/components/Content/Subscribe/Subscribe.js
+++ src/components/Content/Subscribe/Subscribe.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Subscribe.scss';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 const Subscribe = () => {
   return (
     <div className="Subscribe">
-      
+      <Container className="container">
+        <div className="left">
+          <h5>Subscribe To Our Newsletter</h5>
+          <p>Subscribe to our newletter to keep up to date on events,<br />new collections and offers straight to your inbox.</p>
+        </div>
+
+        <div className="right">  
+          <input className="email-input" placeholder="Your email address" />
+        </div>
+      </Container>
     </div>
   );
 }
