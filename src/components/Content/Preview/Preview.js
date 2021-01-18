10571c7b9dff7e1cf0db4798af45fad3580329d4
bigdreamer07 <lancerstar2020@outlook.com>
2021-01-18 16:51:17 -0300

    initial add Preview

---
+++ src/components/Content/Preview/Preview.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import './Preview.scss'
+
+const Preview = (data) => {
+    return (
+        <>
+            Preview
+        </>
+    )
+}
+
+export default Preview
