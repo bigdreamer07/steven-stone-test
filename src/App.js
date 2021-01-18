418ebdb375f8fd0d56234ca5209e1b558a014d89
bigdreamer07 <lancerstar2020@outlook.com>
2021-01-18 10:49:55 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.scss';
+
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
