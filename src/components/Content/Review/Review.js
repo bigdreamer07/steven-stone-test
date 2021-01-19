9dc06aac69b5963a90879e94d70a65b188585b68
bigdreamer07 <lancerstar2020@outlook.com>
2021-01-19 14:57:48 -0300

    review component & style update

--- src/components/Content/Review/Review.js
+++ src/components/Content/Review/Review.js
@@ -3,6 +3,12 @@ import './Review.scss'
 
 import ImgRing from '../../../assets/img/review-ring.jpg';
 import ImgDiamond from '../../../assets/img/review-diamond.png';
+import IconKlarna from '../../../assets/img/klarna.png';
+import IconVisa from '../../../assets/img/payment_visa.png';
+import IconMaster from '../../../assets/img/payment_master.png';
+import IconCard from '../../../assets/img/payment_card.png';
+import IconBank from '../../../assets/img/payment_bank.png';
+
 import IconPen from '@material-ui/icons/BorderColorOutlined';
 
 const Review = (data) => {
@@ -19,23 +25,25 @@ const Review = (data) => {
                             <div className="content-img">
                                 <img src={ImgRing} className="ring-img" />
                             </div>
-                            <div className="content-detail">
-                                <div className="detail-title">
-                                    Diamond Set Lotus
-                                    <span>£530.00</span>
+                            <div className="content-other">
+                                <div className="content-detail">
+                                    <div className="detail-title">
+                                        Diamond Set Lotus
+                                        <span>£530.00</span>
+                                    </div>
+                                    <div className="detail-text">
+                                        <div>Design ID: PRS0516</div>
+                                        <div>Metal: Platinum</div>
+                                        <div>Style: Shoulder Set</div>
+                                        <div>Shape: Princess Cut</div>
+                                        <div>Finger Size:    </div>
+                                        <div>Add Engraving:    </div>
+                                    </div>
                                 </div>
-                                <div className="detail-text">
-                                    <div>Design ID: PRS0516</div>
-                                    <div>Metal: Platinum</div>
-                                    <div>Style: Shoulder Set</div>
-                                    <div>Shape: Princess Cut</div>
-                                    <div>Finger Size:    </div>
-                                    <div>Add Engraving:    </div>
+                                <div className="content-edit">
+                                    <IconPen />
+                                    <span className="underline">change ring design</span>
                                 </div>
-                            </div>
-                            <div className="content-edit">
-                                <IconPen />
-                                <span className="underline">change ring design</span>
                             </div>
                         </div>
                         <div className="content">
@@ -43,27 +51,29 @@ const Review = (data) => {
                             <div className="content-img">
                                 <img src={ImgDiamond} className="diamond-img" />
                             </div>
-                            <div className="content-detail">
-                                <div className="detail-title">
-                                    1.00c Round Diamond
-                                    <span> £261.98</span>
+                            <div className="content-other">
+                                <div className="content-detail">
+                                    <div className="detail-title">
+                                        1.00c Round Diamond
+                                        <span> £261.98</span>
+                                    </div>
+                                    <div className="detail-text">
+                                        <div>Stock Number: RBPUIFSFG</div>
+                                        <div>Carat: 1.00ct</div>
+                                        <div>Colour: H</div>
+                                        <div>Clarity: VS1</div>
+                                        <div>Polish: Very Good</div>
+                                        <div>Symmetry: Excellent</div>
+                                        <div>Cut: Excellent</div>
+                                        <div>Fluorescence: Medium</div>
+                                        <div>Measure (mm): 3.70 x 3.69 x 2.21</div>
+                                        <div className="underline">Certificate: GIA 6321993250</div>
+                                    </div>
                                 </div>
-                                <div className="detail-text">
-                                    <div>Stock Number: RBPUIFSFG</div>
-                                    <div>Carat: 1.00ct</div>
-                                    <div>Colour: H</div>
-                                    <div>Clarity: VS1</div>
-                                    <div>Polish: Very Good</div>
-                                    <div>Symmetry: Excellent</div>
-                                    <div>Cut: Excellent</div>
-                                    <div>Fluorescence: Medium</div>
-                                    <div>Measure (mm): 3.70 x 3.69 x 2.21</div>
-                                    <div className="underline">Certificate: GIA 6321993250</div>
+                                <div className="content-edit">
+                                    <IconPen />
+                                    <span className="underline">change ring design</span>
                                 </div>
-                            </div>
-                            <div className="content-edit">
-                                <IconPen />
-                                <span className="underline">change ring design</span>
                             </div>
                         </div>
                         <div className="content">
@@ -72,22 +82,42 @@ const Review = (data) => {
                                 <div className="subcontent-title">Ring Details</div>
                                 <div className="subcontent-text">
                                     <div>Platinum Simplicity with Round, 0.18ct, I Colour, SI1 Clarity - GIA 46745876</div>
-                                    <div>Ring Size</div>
+                                    <div className="small">Ring Size</div>
                                 </div>
                             </div>
                             <div className="subcontent">
                                 <div className="subcontent-title">Shipping</div>
                                 <div className="subcontent-text">
-                                    <div>Order in 4 hours 3 mins and 30 secs for your order to ship by Thursday, October 22.</div>
-                                    <div>Free Shipping, Hassle-Free Returns</div>
-                                    <div>Need it sooner? Contact Us on 0800 080 3535 or <span className="underline">open online chat</span></div>
+                                    <div>Order in <span className="red">4 hours 3 mins and 30 secs</span> for your order to ship by Thursday, October 22.</div>
+                                    <div className="small">Free Shipping, Hassle-Free Returns</div>
+                                    <div className="small">Need it sooner? Contact Us on 0800 080 3535 or <span className="underline">open online chat</span></div>
                                 </div>
                             </div>
                             <div className="subcontent">
                                 <div className="subcontent-title">Payment options</div>
-                                <div style={{display: "flex"}}>
-
+                                <div className="item-group">
+                                    <div className="subcontent-item">
+                                        <div className="subcontent-image">
+                                            <img src={IconKlarna} />
+                                        </div>
+                                        <div >Pay with finance</div>
+                                        <div className="grey">From £200/mo.</div>
+                                    </div>
+                                    <div> or </div>
+                                    <div className="subcontent-item">
+                                        <div className="subcontent-image">
+                                            <img src={IconBank} />
+                                            <img src={IconMaster} />
+                                            <img src={IconCard} />
+                                            <img src={IconVisa} />
+                                        </div>
+                                        <div >Pay in full</div>
+                                        <div className="grey">£14500</div>
+                                    </div>
                                 </div>
+                                <div className="small underline">Finance Calculator</div>
+                                <div className="small">Payment options are selected at the checkout</div>
+                                <div className="button">Buy Now</div>
                             </div>
                         </div>
                     </div>
