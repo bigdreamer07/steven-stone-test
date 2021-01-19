ff8859a1b6684da40cc50a51ac3a62ade5a6a3b6
bigdreamer07 <lancerstar2020@outlook.com>
2021-01-19 21:24:32 +0800

    review items added

--- src/components/Content/Reviews/Reviews.js
+++ src/components/Content/Reviews/Reviews.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 import './Reviews.scss';
-
+import Slider from "react-slick";
 
 import Stars from '../../../assets/img/stars.png';
 import FeefoLogo from '../../../assets/img/feefo-black.png';
@@ -9,9 +9,17 @@ import Trustpiolt from '../../../assets/img/trustpilot-black.png';
 import Facebook from '../../../assets/img/facebook-black.png';
 import QuoteIcon from '../../../assets/img/quote.png';
 
-const ReviewItem = () => {
+
+var settings = {
+  adaptiveHeight: true,
+  arrows: false,
+  variableWidth: true
+};
+
+const ReviewItem = (props) => {
+  const { grayBg } = props;
   return (
-    <div className="review-item">
+    <div className={"review-item " + (grayBg ? "grayBackground" : "")}>
       <img src={Stars} />
       <span className="h5 name">David Kinsella</span>
       <span className="h6 review-txt">
@@ -31,7 +39,20 @@ const Reviews = () => {
 
   return (
     <div className="Reviews">
-      <ReviewItem />
+      <div className="review-title-container">
+        <span className="title-text">Over 500 5 Star Customer Reviews</span>
+        <span className="title">See What<br />People Say</span>
+        <div className="advertise-logo-container">
+          <a href="/#" className="link"><img alt="feefo" src={FeefoLogo} /></a>
+          <a href="/#" className="link"><img alt="trustpilot" src={Trustpiolt} /></a>
+          <a href="/#" className="link"><img alt="facebook" src={Facebook} /></a>
+        </div>
+      </div>
+
+      <div className="reviews-container">
+        <ReviewItem grayBg={false} />
+        <ReviewItem grayBg={true} />
+      </div>
     </div>
   );
 }
