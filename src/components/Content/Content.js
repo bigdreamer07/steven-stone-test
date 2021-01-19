24f6b37a81240f0ce247726b10642dc65842f747
bigdreamer07 <lancerstar2020@outlook.com>
2021-01-18 23:49:22 -0300

    review component update

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Content.scss';
 
+import Review from './Review/Review';
 import Subscribe from './Subscribe/Subscribe';
 
 const Content = () => {
   return (
     <div className="Content">
+      <Review />
       <Subscribe />
     </div>
   );
